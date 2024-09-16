74944faa410f2b7e73b4bdb03573c78a08030b12
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-16 13:58:05 +0100

    modified if statement

--- Bloomberg/two_sum.py
+++ Bloomberg/two_sum.py
@@ -6,7 +6,7 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         for i in range(len(nums)):
             x = target - nums[i]
-            if x in nums and x != nums[i]:
+            if x in nums and nums.index(x) != i:
                 return [i, nums.index(x)]
             
 
