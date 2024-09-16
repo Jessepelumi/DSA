4d5a6610ea2495cc3d6f28df76a431ed7cfdd85b
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-16 19:16:06 +0100

    added comments to explain code

--- Bloomberg/merge_intervals.py
+++ Bloomberg/merge_intervals.py
@@ -1,15 +1,25 @@
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+# Example
+# Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+# Output: [[1,6],[8,10],[15,18]]
+
 class Solution:
     def mergeIntervals(self, intervals: list[list[int]]) -> list[list[int]]:
         if not intervals:
             return []
         
+        # sort intervals in ascending order using their start times
         intervals.sort(key=lambda x: x[0])
 
+        # list to store merged intervals initialized with the first interval
         merged = [intervals[0]]
 
         for current in intervals[1:]:
+            # last interval in the merged list
             previous = merged[-1]
 
+            # where the end time of an earlier interval is greater than the current
             if current[0] <= previous[1]:
                 previous[1] = max(previous[1], current[1])
             else:
@@ -19,6 +29,7 @@ class Solution:
     
 solution = Solution()
 
+# test cases
 interOne = [[1,3],[2,6],[8,10],[15,18]]
 interTwo = [[1,4],[4,5]]
 
